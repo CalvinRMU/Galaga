444775df36d16941a3a969092477775b7e656200
Calvin Lasswell <calvinlasswell4@gmail.com>
2020-09-10 17:44:53 -0400

    Few Changes

--- Galaga.py
+++ Galaga.py
@@ -2,18 +2,23 @@
 import sys
 import pygame
 
+from settings import Settings
 
 def run_game():
+    #init game, screen, and settings
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    game_settings= Settings()
+
+    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Galaga")
     bg_color = (230, 230, 230)
 
+    #main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(game_settings.bg_color)
         pygame.display.flip()
 
 run_game()
