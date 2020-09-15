6502ac52abc3984f8b5b603b020c2da3f93cccdf
Calvin Lasswell <calvinlasswell4@gmail.com>
2020-09-14 21:22:50 -0400

    movement, wall barriers, refactoring

--- game_functions.py
+++ game_functions.py
@@ -6,14 +6,22 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
+            check_keyup_events(event, ship)
+
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def update_screen(game_settings, screen, ship):
     #Update the screen
--- ship.py
+++ ship.py
@@ -2,10 +2,10 @@ import pygame
 
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, game_settings, screen):
         # init the ship and starting position
         self.screen = screen
-
+        self.game_settings = game_settings
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
@@ -13,15 +13,18 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        self.center= float(self.rect.centerx)
         #movement flags
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        #move based on movement flag
-        if self.moving_right:
-            self.rect.centerx +=1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.game_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.game_settings.ship_speed_factor
 
-
+        self.rect.centerx = self.center
     def blitme(self):
         # draw ship at its current location
         self.screen.blit(self.image, self.rect)
